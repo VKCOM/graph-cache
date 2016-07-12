d7ac628f72d5572443cfd4777e44fe20b3625601
Termina1 <terminal2010@gmail.com>
2016-07-12 11:11:01 +0300

    start tests for utils

---
+++ tests/lib/graph_utils.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const { testGraph } = require('../utils/test_utils');
+
+chai.use(chaiAsPromised);
+const { expect } = chai;
+
+
+const { getDepencies, getDependantLeafs, getLeafSubGraph } = require('../../lib/graph_utils');
+
+describe('getDepencies', () => {
+
+});
+
+describe('getDependantLeafs', () => {
+
+});
+
+describe('getLeafSubGraph', () => {
+
+});
