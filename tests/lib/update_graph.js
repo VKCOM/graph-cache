b7b11f856de71196d70873e68c6a28bacc5da7d8
Termina1 <terminal2010@gmail.com>
2016-07-15 14:41:16 +0300

    styling issues

--- tests/lib/update_graph.js
+++ tests/lib/update_graph.js
@@ -1,17 +1,18 @@
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
-const { loadTestFile, testSign, testGraph, verifyGraph, createPath } = require('../utils/test_utils');
+const { loadTestFile, testSign, testGraph,
+  verifyGraph, createPath } = require('../utils/test_utils');
 
 chai.use(chaiAsPromised);
 
 const { updateGraph } = require('../../lib/update_graph');
 
 function loadFiles(...files) {
-  return Promise.all(files.map(el => loadTestFile(el.toString() + '.txt')));
+  return Promise.all(files.map(el => loadTestFile(`${el.toString()}.txt`)));
 }
 
 function getName(num) {
-  return createPath(num.toString() + '.txt');
+  return createPath(`${num.toString()}.txt`);
 }
 
 
@@ -29,29 +30,29 @@ function load2Graph(info = false) {
 }
 
 function load3Graph(info = false) {
-  return load2Graph(true).then(([g, files, names]) => {
-    return loadFiles(3).then(([[file, name]]) => {
+  return load2Graph(true).then(([g, files, names]) =>
+    loadFiles(3).then(([[file, name]]) => {
       g.setNode(name, testSign(file));
       g.setEdge(getName(2), name);
       if (info) {
         return [g, files.concat([file]), names.concat(name)];
       }
       return g;
-    });
-  });
+    })
+  );
 }
 
 function load3Graph4(info = false) {
-  return load2Graph(true).then(([g, files, names]) => {
-    return loadFiles(4).then(([[file, name]]) => {
+  return load2Graph(true).then(([g, files, names]) =>
+    loadFiles(4).then(([[file, name]]) => {
       g.setNode(name, testSign(file));
       g.setEdge(name, getName(1));
       if (info) {
         return [g, files.concat([file]), names.concat(name)];
       }
       return g;
-    });
-  });
+    })
+  );
 }
 
 function load4Graph(info = false) {
@@ -103,10 +104,10 @@ describe('updateGraph', () => {
     )
   );
 
-  it('adds new node if it appeared in graph', () => {
-    return loadFiles(4).then(([[file4, name4]]) => {
-      return load3Graph(true).then(([g, files, names]) => {
-        return updateGraph(g, testSign, () => load3Graph4(), '', getName(1))
+  it('adds new node if it appeared in graph', () =>
+    loadFiles(4).then(([[file4, name4]]) =>
+      load3Graph(true).then(([g, files, names]) =>
+        updateGraph(g, testSign, () => load3Graph4(), '', getName(1))
           .then(nwg =>
             verifyGraph(nwg, [
               [names[0], testSign(files[0])],
@@ -119,14 +120,14 @@ describe('updateGraph', () => {
               { v: name4, w: names[0] },
             ]
           )
-        );
-      });
-    });
-  });
+        )
+      )
+    )
+  );
 
-  it('removes old node and edges if dep was remove', () => {
-    return load4Graph(true).then(([g, files, names]) => {
-      return updateGraph(g, testSign, () => load2Graph(), '', getName(1))
+  it('removes old node and edges if dep was remove', () =>
+    load4Graph(true).then(([g, files, names]) =>
+      updateGraph(g, testSign, () => load2Graph(), '', getName(1))
         .then(nwg =>
           verifyGraph(nwg, [
             [names[0], testSign(files[0])],
@@ -137,20 +138,19 @@ describe('updateGraph', () => {
             { v: names[1], w: names[2] },
           ]
         )
-      );
-    });
-  });
+      )
+    )
+  );
 
-  it('changes graph structure if dependency graph changed', () => {
-    return load3Graph(true).then(([g, files, names]) => {
+  it('changes graph structure if dependency graph changed', () =>
+    load3Graph(true).then(([g, files, names]) => {
       g.removeEdge(getName(2), getName(3));
       g.setEdge(getName(3), getName(1));
       return updateGraph(g, testSign, () => load3Graph().then(nwg => {
         nwg.removeEdge(getName(2), getName(3));
         nwg.setEdge(getName(3), getName(2));
         return nwg;
-      }), '', getName(1))
-        .then(nwg =>
+      }), '', getName(1)).then(nwg =>
           verifyGraph(nwg, [
             [names[0], testSign(files[0])],
             [names[1], testSign(files[1])],
@@ -161,12 +161,12 @@ describe('updateGraph', () => {
           ]
         )
       );
-    });
-  });
+    })
+  );
 
-  it('if subgraph is empty, will remove only leaf', () => {
-    return load3Graph(true).then(([g, files, names]) => {
-      return updateGraph(g, testSign, () => Promise.resolve(testGraph()), '', getName(1))
+  it('if subgraph is empty, will remove only leaf', () =>
+    load3Graph(true).then(([g, files, names]) =>
+      updateGraph(g, testSign, () => Promise.resolve(testGraph()), '', getName(1))
         .then(nwg =>
           verifyGraph(nwg, [
             [names[1], testSign(files[1])],
@@ -175,16 +175,14 @@ describe('updateGraph', () => {
             { v: names[1], w: names[2] },
           ]
         )
-      );
-    });
-  });
+      )
+    )
+  );
 
-  it('if subgraph is empty, will remove leaf and nodes with no connection', () => {
-    return load2Graph(true).then(([g, files, names]) => {
-      return updateGraph(g, testSign, () => Promise.resolve(testGraph()), '', getName(1))
-        .then(nwg =>
-          verifyGraph(nwg, [], [])
-        );
-    });
-  });
+  it('if subgraph is empty, will remove leaf and nodes with no connection', () =>
+    load2Graph(true).then(([g]) =>
+      updateGraph(g, testSign, () => Promise.resolve(testGraph()), '', getName(1))
+        .then(nwg => verifyGraph(nwg, [], []))
+    )
+  );
 });
